ac1dae6c41578b36426898cdfadda6d268ffd1d7
Lucas José <103715932+jLucasAS@users.noreply.github.com>
2025-02-25 20:21:05 -0300

    Add files via upload

--- primeiro trunfo iniciante.c
+++ primeiro trunfo iniciante.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
     int main(){
-
+        //Variaveis das cartas.
         char estadoA[3], estadoB[3];
         char codigoA[5], codigoB[5];
         char nomedacidadeA[20], nomedacidadeB[20];
@@ -8,46 +8,51 @@
         int pontosturisticos1, pontosturisticos2;
 
         //Cadastro da carta numero 1.
-        printf("insira o codigo da carta 1: \n");
+        printf("insira o codigo da carta 1 (exemplo: C1):");
         scanf("%s", codigoA);
-        
-        printf("insira o nome da cidade: \n");
+        getchar(); //limpeza do buffer.
+
+        printf("insira o nome da cidade:");
         scanf("%s", nomedacidadeA);
-        
-        printf("insira o estado: \n");
+        getchar();
+
+        printf("insira a sigla do estado: (exemplo: DF):");
         scanf("%s", estadoA);
-       
-        printf("insira a população: \n ");
+        getchar();
+
+        printf("insira a população:");
         scanf("%f", &populacao1);
-        
-        printf("insira o PIB: \n");
+        getchar();
+
+        printf("insira o PIB:");
         scanf("%f", &pib1);
-        
-        printf("insira o numero de pontos turisticos: \n");
+        getchar();
+
+        printf("insira o numero de pontos turisticos:");
         scanf("%d", &pontosturisticos1);
 
         // cadastro da carta numero 2
         
-        printf("insira o codigo da carta 2: \n");
+        printf("insira o codigo da carta 2:");
         scanf("%s", codigoB);
 
-        printf("insira o nome da cidade: \n");
+        printf("insira o nome da cidade:");
         scanf("%s", nomedacidadeB);
 
-        printf("Insira o estado: \n");
+        printf("Insira o estado:");
         scanf("%s", estadoB);
 
-        printf("insira o numero da população: \n");
+        printf("insira o numero da população:");
         scanf("%f", &populacao2);
 
-        printf("insira o Pib: \n");
+        printf("insira o Pib:");
         scanf("%f", &pib2);
 
-        printf("insira o numero de pontos turisticos: \n");
+        printf("insira o numero de pontos turisticos:");
         scanf("%d", &pontosturisticos2);
 
-        //Exibição das informaçoes das cartas 1 e 2.
-
+        //Exibição das informaçoes das cartas.
+        //carta 1.
         printf("codigo da carta 1: %s \n", codigoA);
         printf("nome da cidade: %s \n", nomedacidadeA);
         printf("Estado: %s \n", estadoA);
@@ -55,6 +60,7 @@
         printf("Pib: %.3f \n", pib1);
         printf("Pontos Turisticos: %d \n", pontosturisticos1);
 
+        //carta 2.
         printf("Codigo da carta 2: %s \n", codigoB);
         printf("Nome da cidade: %s \n", nomedacidadeB);
         printf("Estado: %s \n", estadoB);
